7095747df69c46ba97bdd2680291a9d01e5430d7
Zhe Zhang <zhezhangsh@users.noreply.github.com>
2016-03-06 18:30:51 -0500

    Create IndexStarReference.sh

---
+++ examples/IndexStarReference.sh
@@ -0,0 +1,9 @@
+# Index reference genome for STAR alignment
+# No gene annotation information
+/nas/is1/rnaseq_workspace/tools/STAR-2.5.1b/bin/Linux_x86_64_static/STAR \
+--runThreadN 12 \
+--runMode genomeGenerate \
+--genomeDir /nas/is1/rnaseq_workspace/refs/mm38/star \
+--genomeFastaFiles /nas/is1/rnaseq_workspace/refs/mm38/GCF_000001635.24_GRCm38.p4_genomic.fna
+
+
